609df27d2f9d28e6adbf91f3c1326c682d37861b
Yury Zhloba <yzh44yzh@yandex.ru>
2026-06-21 21:28:01 +0300

    generate pairs

--- go_math.go
+++ go_math.go
@@ -3,7 +3,13 @@ package main
 import (
 	"flag"
 	"fmt"
+	"math/rand"
 )
+
+type Pair struct {
+	x int
+	y int
+}
 
 func main() {
 	var range_from int
@@ -15,9 +21,27 @@ func main() {
 	flag.IntVar(&range_to, "t", 9, "range right border (shorthand)")
 
 	var limit int
-	flag.IntVar(&limit, "limit", 10, "number of questions")
-	flag.IntVar(&limit, "l", 10, "number of questions (shorthand)")
+	flag.IntVar(&limit, "limit", 20, "number of questions")
+	flag.IntVar(&limit, "l", 20, "number of questions (shorthand)")
 
 	flag.Parse()
 	fmt.Println("from", range_from, "to", range_to, "limit", limit)
+
+	pairs := make_pairs(range_from, range_to)
+	rand.Shuffle(len(pairs), func(i, j int) {
+		pairs[i], pairs[j] = pairs[j], pairs[i]
+	})
+	limit = min(len(pairs), limit)
+	pairs = pairs[:limit]
+	fmt.Println("pairs", pairs)
+}
+
+func make_pairs(range_from, range_to int) []Pair {
+	pairs := []Pair{}
+	for x := range_from; x <= range_to; x++ {
+		for y := range_from; y <= range_to; y++ {
+			pairs = append(pairs, Pair{x, y})
+		}
+	}
+	return pairs
 }
